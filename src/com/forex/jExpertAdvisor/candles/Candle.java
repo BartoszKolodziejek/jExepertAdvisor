7f0d21180cace64c420dc46e1802d7584a806e51
unknown <bartosz.kolodziejek@gmail.com>
2018-02-11 18:19:08 +0100

    candles changed

--- src/com/forex/jExpertAdvisor/candles/Candle.java
+++ src/com/forex/jExpertAdvisor/candles/Candle.java
@@ -1,6 +1,7 @@
 package com.forex.jExpertAdvisor.candles;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 public class Candle {
@@ -9,47 +10,40 @@ public class Candle {
 	private BigDecimal close;
 	private BigDecimal high;
 	private BigDecimal low;
-	boolean currentCandle;
+	private Date date;
 	List<Candle> subCandles;
 	public BigDecimal getOpen() {
 		return open;
 	}
-	public void setOpen(BigDecimal open) {
-		this.open = open;
-	}
 	public BigDecimal getClose() {
 		return close;
-	}
-	public void setClose(BigDecimal close) {
-		this.close = close;
 	}
 	public BigDecimal getHigh() {
 		return high;
 	}
-	public void setHigh(BigDecimal high) {
-		this.high = high;
-	}
 	public BigDecimal getLow() {
 		return low;
 	}
-	public void setLow(BigDecimal low) {
-		this.low = low;
+	public Date getDate() {
+		return date;
 	}
-	public boolean isCurrentCandle() {
-		return currentCandle;
-	}
-	public void setCurrentCandle(boolean currentCandle) {
-		
-		if(!currentCandle)
-			subCandles = null;
-		this.currentCandle = currentCandle;
-	}
+	
 	public List<Candle> getSubCandles() {
 		return subCandles;
 	}
-	public void setSubCandles(List<Candle> subCandles) {
+	public Candle(BigDecimal open, BigDecimal close, BigDecimal high, BigDecimal low, Date date,
+			List<Candle> subCandles) {
+		super();
+		this.open = open;
+		this.close = close;
+		this.high = high;
+		this.low = low;
+		this.date = date;
 		this.subCandles = subCandles;
 	}
+	
+	
+	
 	
 	
 	
