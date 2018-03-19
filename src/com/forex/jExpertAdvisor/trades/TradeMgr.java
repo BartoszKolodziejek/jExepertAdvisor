6d48c78e47c01cb59fa3878c8e28479bbd1d2bc2
bartosz.kolodziejek@gmail.com <696523893aA>
2018-03-19 20:58:01 +0100

    closing issue

--- src/com/forex/jExpertAdvisor/trades/TradeMgr.java
+++ src/com/forex/jExpertAdvisor/trades/TradeMgr.java
@@ -102,6 +102,7 @@ public  class TradeMgr implements ITradesMgr {
 		params.put("strategyName", trade.getStrategy().getClass().getName());
 		params.put("close",simpleDateFormat.format(MarketMgr.getInstance(trade.getSymbol()).getCurrentCandle().getDate()));
 		WebQuerySender.getInstance().send("http://localhost:8090/closeTrade", params);
+		ExistingTrades.getInstance().remove(trade);
 
 		
 
